87e5bed1406cd90a669ae8b5edac9eb0bd7b2e42
Allan JEGU <allanjegu@gmail.com>
2021-05-01 22:59:08 +0200

    ALC-65 Use AWS SAM for deploy

--- aws/codebuild/s3.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "s3_bucket_allcoin_track_artifact" {
-  bucket = "allcoin-track-artifact"
-}
-
-resource "aws_s3_bucket" "s3_bucket_allcoin_track_cache" {
-  bucket = "allcoin-track-cache"
-}
